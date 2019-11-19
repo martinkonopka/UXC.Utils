1d33fcc61a6b872311e1b1f9d5faa991cf694aec
Martin Konopka <martkono@outlook.com>
2019-11-19 19:59:18 +0100

    added logic for invalidating zero pos gaze samples

---
+++ src/UXC.Utils.InvalidateZeroPosGazeSamples/Options/FilterOptions.cs
@@ -0,0 +1,42 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UXI.Filters.Options;
+using UXI.Serialization;
+
+namespace UXC.Utils.InvalidateZeroPosGazeSamples
+{
+    public class FilterOptions
+        : IInputOptions
+        , IOutputOptions
+    {
+        [Value(0, HelpText = "Path to the input gaze data file. If omitted, standard input stream is used.", MetaName = "input file", MetaValue = "FILE", Required = false)]
+        public virtual string InputFilePath { get; set; }
+
+
+        [Option("format", Default = FileFormat.Default, HelpText = "Data format of the input.")]
+        public virtual FileFormat InputFileFormat { get; set; }
+
+
+        public virtual FileFormat DefaultInputFileFormat => FileFormat.JSON;
+
+
+        [Option('o', "output", Default = null, HelpText = "Path to the output file. If omitted, standard output stream is used.", Required = false)]
+        public virtual string OutputFilePath { get; set; }
+
+
+        [Option("output-format", Default = FileFormat.Default, HelpText = "Data format of the output.", Required = false)]
+        public virtual FileFormat OutputFileFormat { get; set; }
+
+
+        public virtual FileFormat DefaultOutputFileFormat => FileFormat.JSON;
+
+
+
+        [Option('t', "threshold", Default = 0.1d, HelpText = "Minimum distance of positions in data to the zero position.", Required = false)]
+        public virtual double Threshold { get; set; }
+    }
+}
--- src/UXC.Utils.InvalidateZeroPosGazeSamples/Program.cs
+++ src/UXC.Utils.InvalidateZeroPosGazeSamples/Program.cs
@@ -1,15 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UXC.Core.Data;
+using UXC.Core.Data.Compatibility.GazeToolkit;
+using UXC.Utils.InvalidateZeroPosGazeSamples.Configuration;
+using UXI.Filters;
+using UXI.Filters.Configuration;
+using UXI.GazeToolkit.Utils;
 
 namespace UXC.Utils.InvalidateZeroPosGazeSamples
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            return new SingleFilterHost<FilterOptions>
+            (
+                new RelayFilter<GazeData, GazeData, FilterOptions>("Invalidate Gaze Data Samples with Zero Positions", Invalidate),
+                new UXCDataSerializationFilterConfiguration()
+            ).Execute(args);
+        }
+
+
+        private static IObservable<GazeData> Invalidate(IObservable<GazeData> data, FilterOptions options, FilterContext context)
+        {
+            EyeGazeData invalidData = new EyeGazeData(EyeGazeDataValidity.Invalid, Point2.Default, Point3.Default, Point3.Default, Point3.Default, 0);
+
+            return data.Select(gaze =>
+            {
+                var leftEye = HasZeroPosition(gaze.LeftEye, options.Threshold) ? invalidData : gaze.LeftEye;
+                var rightEye = HasZeroPosition(gaze.RightEye, options.Threshold) ? invalidData : gaze.RightEye;
+
+                return new GazeData
+                (
+                    EyeValidityEx2.MergeToEyeValidity(leftEye.Validity, rightEye.Validity),
+                    leftEye,
+                    rightEye,
+                    gaze.TrackerTicks,
+                    gaze.Timestamp    
+                );
+            });
+        }
+
+
+        private static bool HasZeroPosition(EyeGazeData eye, double thresholdDistance)
+        {
+            return PointUtils.EuclidianDistance(eye.GazePoint3D.ToToolkit(), UXI.GazeToolkit.Point3.Zero) <= thresholdDistance
+                || PointUtils.EuclidianDistance(eye.EyePosition3D.ToToolkit(), UXI.GazeToolkit.Point3.Zero) <= thresholdDistance;
         }
     }
 }
